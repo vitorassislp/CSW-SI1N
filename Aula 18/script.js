fac5e41aa4f886c3d40e843fea89ed20ea7bb4de
vitorassislp <vitorleppaus@gmail.com>
2025-05-13 20:43:08 -0300

    Add files via upload

---
+++ Aula 18/script.js
@@ -0,0 +1,4 @@
+        document.write("<h3>Olá, Mundo!</h3t>");
+        window.alert("Olá, Mundo!")
+        document.getElementById("resposta").innerHTML = window.confirm("Você está gostando do curso?")
+        document.getElementById("resposta").innerHTML = "O meu nome é " + window.prompt("Qual é o seu nome?");
---
+++ Aula 18/script2.js
@@ -0,0 +1,4 @@
+function enviarDados()
+{
+document.getElementById("inputusuario").innerHTML = "Nome enviado foi: " + window.prompt ("Digite seu nome: ")
+}
